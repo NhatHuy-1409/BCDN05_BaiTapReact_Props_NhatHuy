6d58b0c4b43ef1921e3a5aaf380f6775fba1e77f
Jacksanxx <nhathuy97ars@gmail.com>
2022-03-12 15:05:37 +0700

    change https

--- src/Components/ShoesStore.js
+++ src/Components/ShoesStore.js
@@ -13,7 +13,7 @@ export default class ShoesStore extends Component {
             "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
             "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
             "quantity": 995,
-            "image": "http://svcy3.myclass.vn/images/adidas-prophere.png"
+            "image": "https://svcy3.myclass.vn/images/adidas-prophere.png"
         },
         {
             "id": 2,
@@ -23,7 +23,7 @@ export default class ShoesStore extends Component {
             "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
             "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
             "quantity": 990,
-            "image": "http://svcy3.myclass.vn/images/adidas-prophere-black-white.png"
+            "image": "https://svcy3.myclass.vn/images/adidas-prophere-black-white.png"
         },
         {
             "id": 3,
@@ -33,7 +33,7 @@ export default class ShoesStore extends Component {
             "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
             "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
             "quantity": 415,
-            "image": "http://svcy3.myclass.vn/images/adidas-prophere-customize.png"
+            "image": "https://svcy3.myclass.vn/images/adidas-prophere-customize.png"
         },
         {
             "id": 4,
@@ -43,7 +43,7 @@ export default class ShoesStore extends Component {
             "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
             "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
             "quantity": 542,
-            "image": "http://svcy3.myclass.vn/images/adidas-super-star-red.png"
+            "image": "https://svcy3.myclass.vn/images/adidas-super-star-red.png"
         },
         {
             "id": 5,
@@ -73,7 +73,7 @@ export default class ShoesStore extends Component {
             "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
             "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
             "quantity": 854,
-            "image": "http://svcy3.myclass.vn/images/adidas-ultraboost-4.png"
+            "image": "https://svcy3.myclass.vn/images/adidas-ultraboost-4.png"
         },
         {
             "id": 8,
@@ -83,7 +83,7 @@ export default class ShoesStore extends Component {
             "description": "The adidas Primeknit upper wraps the foot with a supportive fit that enhances movement.\r\n\r\n",
             "shortDescription": "The midsole contains 20% more Boost for an amplified Boost feeling.\r\n\r\n",
             "quantity": 524,
-            "image": "http://svcy3.myclass.vn/images/adidas-yeezy-350.png"
+            "image": "https://svcy3.myclass.vn/images/adidas-yeezy-350.png"
         },
         {
             "id": 9,
@@ -93,7 +93,7 @@ export default class ShoesStore extends Component {
             "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
             "shortDescription": "Paul George is the rare high-percentage shooter",
             "quantity": 599,
-            "image": "http://svcy3.myclass.vn/images/nike-adapt-bb.png"
+            "image": "https://svcy3.myclass.vn/images/nike-adapt-bb.png"
         },
         {
             "id": 10,
@@ -103,7 +103,7 @@ export default class ShoesStore extends Component {
             "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
             "shortDescription": "Paul George is the rare high-percentage shooter",
             "quantity": 984,
-            "image": "http://svcy3.myclass.vn/images/nike-air-max-97.png"
+            "image": "https://svcy3.myclass.vn/images/nike-air-max-97.png"
         },
         {
             "id": 11,
@@ -113,7 +113,7 @@ export default class ShoesStore extends Component {
             "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
             "shortDescription": "Paul George is the rare high-percentage shooter",
             "quantity": 875,
-            "image": "http://svcy3.myclass.vn/images/nike-air-max-97-blue.png"
+            "image": "https://svcy3.myclass.vn/images/nike-air-max-97-blue.png"
         },
         {
             "id": 12,
@@ -123,7 +123,7 @@ export default class ShoesStore extends Component {
             "description": "Nike shoe is the rare high-percentage shooter who's also a coach's dream on D. Designed for his unrivaled 2-way game, the PG 4 unveils a new cushioning system that's lightweight, articulated and responsive, ideal for players like PG who go hard every play.\r\n\r\n",
             "shortDescription": "Paul George is the rare high-percentage shooter",
             "quantity": 445,
-            "image": "http://svcy3.myclass.vn/images/nike-air-max-270-react.png"
+            "image": "https://svcy3.myclass.vn/images/nike-air-max-270-react.png"
         }
     ]
 
